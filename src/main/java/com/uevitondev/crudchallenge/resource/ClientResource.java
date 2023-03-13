c4f7fa52c58cadcaf76c4f788d0960fa52892d3e
uevitondev <uevitonsantos@outlook.com>
2023-03-13 16:22:58 -0300

    GET Client paged

--- src/main/java/com/uevitondev/crudchallenge/resource/ClientResource.java
+++ src/main/java/com/uevitondev/crudchallenge/resource/ClientResource.java
@@ -1,8 +1,15 @@
 package com.uevitondev.crudchallenge.resource;
 
+import com.uevitondev.crudchallenge.dto.ClientDTO;
 import com.uevitondev.crudchallenge.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +18,18 @@ public class ClientResource {
 
     @Autowired
     private ClientService clientService;
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAllClientPaged(
+            @RequestParam(name = "page", defaultValue = "0") Integer page,
+            @RequestParam(name = "linesPerPage", defaultValue = "5") Integer linesPerPage,
+            @RequestParam(name = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(name = "orderBy", defaultValue = "name") String orderBy
+    ) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        Page<ClientDTO> allClientPaged = clientService.findAllClientPaged(pageRequest);
+
+        return ResponseEntity.ok().body(allClientPaged);
+    }
 
 }
--- src/main/java/com/uevitondev/crudchallenge/service/ClientService.java
+++ src/main/java/com/uevitondev/crudchallenge/service/ClientService.java
@@ -1,7 +1,10 @@
 package com.uevitondev.crudchallenge.service;
 
+import com.uevitondev.crudchallenge.dto.ClientDTO;
 import com.uevitondev.crudchallenge.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,5 +12,9 @@ public class ClientService {
 
     @Autowired
     private ClientRepository clientRepository;
+
+    public Page<ClientDTO> findAllClientPaged(PageRequest pageRequest) {
+        return clientRepository.findAll(pageRequest).map(ClientDTO::new);
+    }
 
 }
