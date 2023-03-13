245da5cc6a01f07dba0c74fe1db8aba820dd1273
uevitondev <uevitonsantos@outlook.com>
2023-03-13 16:45:08 -0300

    GET Client ById

--- src/main/java/com/uevitondev/crudchallenge/resource/ClientResource.java
+++ src/main/java/com/uevitondev/crudchallenge/resource/ClientResource.java
@@ -7,7 +7,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/clients")
@@ -28,11 +31,5 @@ public class ClientResource {
 
         return ResponseEntity.ok().body(allClientPaged);
     }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<ClientDTO> findClientById(@PathVariable Long id) {
-        return ResponseEntity.ok().body(clientService.findClientById(id));
-    }
-
 
 }
--- src/main/java/com/uevitondev/crudchallenge/service/ClientService.java
+++ src/main/java/com/uevitondev/crudchallenge/service/ClientService.java
@@ -1,15 +1,11 @@
 package com.uevitondev.crudchallenge.service;
 
 import com.uevitondev.crudchallenge.dto.ClientDTO;
-import com.uevitondev.crudchallenge.entity.Client;
 import com.uevitondev.crudchallenge.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.NoSuchElementException;
 
 @Service
 public class ClientService {
@@ -17,17 +13,8 @@ public class ClientService {
     @Autowired
     private ClientRepository clientRepository;
 
-    @Transactional(readOnly = true)
     public Page<ClientDTO> findAllClientPaged(PageRequest pageRequest) {
         return clientRepository.findAll(pageRequest).map(ClientDTO::new);
     }
-
-    @Transactional(readOnly = true)
-    public ClientDTO findClientById(Long id) {
-        Client client = clientRepository.findById(id)
-                .orElseThrow(() -> new NoSuchElementException("Entity not found! id: " + id));
-        return new ClientDTO(client);
-    }
-
 
 }
