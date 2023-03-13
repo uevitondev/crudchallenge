65877dae6a3f11db3a3dae6905b2a21831b1a576
uevitondev <uevitonsantos@outlook.com>
2023-03-13 16:50:54 -0300

    fix

--- src/main/java/com/uevitondev/crudchallenge/resource/ClientResource.java
+++ src/main/java/com/uevitondev/crudchallenge/resource/ClientResource.java
@@ -7,10 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/clients")
@@ -31,5 +28,11 @@ public class ClientResource {
 
         return ResponseEntity.ok().body(allClientPaged);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClientDTO> findClientById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(clientService.findClientById(id));
+    }
+
 
 }
